5c06dca9ee79a4142fc30485cf5019c5a6d4d2a4
schragman <schragman@gmail.com>
2025-03-13 23:23:01 +0100

    Date- und Zeitpicker funktionieren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  MatDrawer,
+  MatDrawerContainer, MatDrawerContent,
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent
+} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MatFormField, MatHint} from '@angular/material/form-field';
+import {provideNativeDateAdapter} from '@angular/material/core';
+import {FormsModule} from '@angular/forms';
+import {ContainerComponent} from './container/container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ContainerComponent,
+    FormsModule,
+    MatSidenavContainer,
+    MatToolbar,
+    MatIcon,
+    MatIconButton,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [provideNativeDateAdapter()],
+  standalone: true
 })
 export class AppComponent {
   title = 'Arbeitserfassung';
+
+  @ViewChild(ContainerComponent) containerComponent!: ContainerComponent
+
+  onClickBurger() {
+    this.containerComponent.openMenu();
+  }
 }
---
+++ src/app/helpers/formats.ts
@@ -0,0 +1,12 @@
+import { MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+export const DE_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD.MM.YYYY',
+  },
+  display: {
+    dateInput: 'DD.MM.YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
